1eeb68a90455be5e86d6f2b831acfe90523e453b
Martin Gras <martin.gras95@gmail.com>
2020-01-08 21:52:46 +0000

    Updates

---
+++ precache-manifest.4d7f7776637f3e861b91daf3642bf708.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "845c8d2f8ca952994ded952b61013a4a",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "a4539cbcd74353236b56",
+    "url": "/testapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d0e307f79012bd6a7c3c",
+    "url": "/testapp/static/js/2.33313210.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/testapp/static/js/2.33313210.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4539cbcd74353236b56",
+    "url": "/testapp/static/js/main.eec6c769.chunk.js"
+  },
+  {
+    "revision": "dbc4a55a6458edb9930a",
+    "url": "/testapp/static/js/runtime-main.d1a284a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
